c1ad2b2e58f4498c7805f0800f21c9458b1c0ea3
Daniar <zhylangozov.daniar@gmail.com>
2024-09-17 01:18:00 -0400

    5th commit - part 1 finished

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -50,4 +50,88 @@ function setBoardCell(board, letter, row, col){
     return boardCopy;
 }
 
-export {generateBoard, boardFromString, rowColToIndex, indexToRowCol, setBoardCell}
+function algebraicToRowCol(algebraicNotation){
+    const reg = /^[A-Z](1[0-9]|2[0-6]|[1-9])$/;
+
+    const result = algebraicNotation.match(reg);
+
+    if (result == null){
+        return undefined;
+    }
+    else{
+        let col = parseInt(result[0][1]) - 1;
+        let row = result[0][0].charCodeAt(0) - 65
+
+        return {"row": row, "col": col}
+    }
+
+}
+
+function placeLetter(board, letter, algebraicNotation){
+    let coordinates = algebraicToRowCol(algebraicNotation);
+    return setBoardCell(board, letter, coordinates.row, coordinates.col);
+}
+
+function getWinner(board){
+    function checkWinner(player, row, col) {
+        const dimension = Math.sqrt(board.length);
+
+        if (board.slice(row * dimension, row * dimension + dimension).every(cell => cell === player)) {
+            return true;
+        }
+
+        if (Array.from({ length: dimension }, (_, i) => board[i * dimension + col]).every(cell => cell === player)) {
+            return true;
+        }
+
+        if (row === col && Array.from({ length: dimension }, (_, i) => board[i * (dimension + 1)]).every(cell => cell === player)) {
+            return true;
+        }
+
+        if (row + col === dimension - 1 && Array.from({ length: dimension }, (_, i) => board[(i + 1) * (dimension - 1)]).every(cell => cell === player)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    const dimension = Math.sqrt(board.length);
+    for (let row = 0; row < dimension; row++) {
+        for (let col = 0; col < dimension; col++) {
+            const player = board[row * dimension + col];
+            if (player !== ' ' && checkWinner(player, row, col)) {
+                return player;
+            }
+        }
+    }
+
+    return undefined; 
+}
+
+function isBoardFull(board){
+    for(let i = 0; i < board.length; i++){
+        if(board[i] == ' '){
+            return false
+        }
+    }
+    return true;
+}
+
+function isValidMove(board, algebraicNotation){
+    let coordinates = algebraicToRowCol(algebraicNotation)
+    if (coordinates == undefined){
+        return false
+    }
+
+    let index = rowColToIndex(board, coordinates.row, coordinates.col);
+
+    if(board[index] == " "){
+        return true;
+    }
+    else{
+        return false
+    }
+}
+
+export {generateBoard, boardFromString, rowColToIndex, indexToRowCol, setBoardCell, algebraicToRowCol, placeLetter, getWinner,
+isBoardFull, isValidMove}
